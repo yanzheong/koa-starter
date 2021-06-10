45a6133491176338eba88c69c53409314a54fa50
haiauth <85645968+haiauth@users.noreply.github.com>
2021-06-10 18:16:10 -0400

    Update captchaRetrieve.js
    controllers are done

--- controllers/captchaRetrieve.js
+++ controllers/captchaRetrieve.js
@@ -1,1 +1,46 @@
-// This file will have code to request a problem from the backend + receive it
+const config = require("../config.json");
+const captchaModel = require("../models/captcha")
+
+module.exports.createACaptcha = async(ctx) => {
+
+	let user;
+
+	if (ctx.isAuthenticated()) {
+		user = ctx.session.passport.user;
+	} else {
+		return ctx.redirect("/");
+	}
+
+    const questionArr = captchaModel.createCAPTCHA(user);
+
+	await ctx.render("captcha", {
+		title: config.site.name,
+		user: user,
+        array: questionArr
+	});
+};
+
+
+
+module.exports.checkForErr = async(ctx) => {
+
+	let user;
+    
+	if (ctx.isAuthenticated()) {
+		user = ctx.session.passport.user;
+	} else {
+		return ctx.redirect("/");
+	}
+
+    const userAnswer = ctx.request.body;
+
+    if (!userAnswer) {
+		throw new Error("Missing parameter object");
+	}
+
+    const result = captchaModel.checkCAPTCHA(user, userAnswer);
+
+    if(result == "WRONG"){
+        throw new Error(result);
+    }
+};
